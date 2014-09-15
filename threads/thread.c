1702d858686e2f3f972a680aa206eacb816a8560
Paras <paraschauhan26@gmail.com>
2014-09-15 16:20:15 -0400

    Space removed

--- threads/thread.c
+++ threads/thread.c
@@ -14,8 +14,7 @@
 #ifdef USERPROG
 #include "userprog/process.h"
 #endif
-
-
+// iske neeche ka line nikaala
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
    of thread.h for details. */
